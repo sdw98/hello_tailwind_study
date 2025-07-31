b186cdf65a1019bd0432fd8cfcd80bf1fae3b0c6
sdw98 <sdw9851@gmail.com>
2025-07-31 09:19:44 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Calendar from "./components/Calendar";
+import DateCard from "./components/DateCard";
+
+const App = () => {
+  return (
+    <div className="bg-red-100 min-h-screen flex justify-center items-center">
+      <Calendar />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/DateCard.jsx
@@ -0,0 +1,15 @@
+const DateCard = () => {
+  return (
+    <div className="bg-white w-40 h-52 rounded-xl">
+      <div className="bg-red-400 text-2xl font-semibold text-right p-2 rounded-t-xl">
+        15
+      </div>
+      <div className="text-lg p-2 flex flex-col gap-2">
+        <p>🇰🇷 광복절</p>
+        <p>🧹 청소하기</p>
+      </div>
+    </div>
+  );
+};
+
+export default DateCard;
